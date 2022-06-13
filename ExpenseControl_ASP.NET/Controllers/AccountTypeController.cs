d9e9289e9daae2098c8428687ab7a0e8b7d9c23f
luisJavierLuna <javierlunaluis@gmail.com>
2022-06-12 20:01:08 -0500

    Creating the first model, view and controller

---
+++ ExpenseControl_ASP.NET/Controllers/AccountTypeController.cs
@@ -0,0 +1,19 @@
+﻿using ExpenseControl_ASP.NET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseControl_ASP.NET.Controllers
+{
+    public class AccountTypeController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(AccountType accountType)
+        {
+            return View();
+        }
+    }
+}
---
+++ ExpenseControl_ASP.NET/Models/AccountType.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseControl_ASP.NET.Models
+{
+    public class AccountType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public int Order { get; set; }
+    }
+}
