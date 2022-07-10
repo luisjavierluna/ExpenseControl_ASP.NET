b29f370ea20d0bde56d731ccdd42ab08028fd2fd
luisJavierLuna <javierlunaluis@gmail.com>
2022-07-10 16:44:26 -0500

    Created basic functions for Create Category method, P1: added repository

---
+++ ExpenseControl_ASP.NET/Services/CategoriesRepository.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using ExpenseControl_ASP.NET.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ExpenseControl_ASP.NET.Services
+{
+    public interface ICategoriesRepository
+    {
+        Task Create(Category category);
+    }
+
+    public class CategoriesRepository: ICategoriesRepository
+    {
+        private readonly string connectionString;
+
+        public CategoriesRepository(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Create(Category category)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(@"
+                INSERT INTO Categories(Name, OperationTypeId, UserId)
+                VALUES(@Name, @OperationTypeId, @UserId)
+                SELECT SCOPE_IDENTITY();",
+                category);
+            category.Id = id;
+        }
+
+    }
+}
