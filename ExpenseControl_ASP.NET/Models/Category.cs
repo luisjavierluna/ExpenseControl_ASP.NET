dc377f33cebadc125c57c99db9b2b52dc83d14ed
luisJavierLuna <javierlunaluis@gmail.com>
2022-07-10 16:32:51 -0500

    Created basic functions for Create Category method, P1: added models

---
+++ ExpenseControl_ASP.NET/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseControl_ASP.NET.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [StringLength(maximumLength: 50, ErrorMessage = "This field cannot be longer than {1} characters")]
+        public string Name { get; set; }
+        public OperationType OperationTypeId { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ ExpenseControl_ASP.NET/Models/OperationType.cs
@@ -0,0 +1,8 @@
+﻿namespace ExpenseControl_ASP.NET.Models
+{
+    public enum OperationType
+    {
+        Income = 1,
+        Expense = 2
+    }
+}
