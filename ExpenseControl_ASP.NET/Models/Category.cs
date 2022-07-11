3f5195d76a48f38ffaa8056fe6be65f760fa19f1
luisJavierLuna <javierlunaluis@gmail.com>
2022-07-11 14:36:38 -0500

    Created basic functions for Create Category method, P4: added view

--- ExpenseControl_ASP.NET/Models/Category.cs
+++ ExpenseControl_ASP.NET/Models/Category.cs
@@ -7,6 +7,7 @@ namespace ExpenseControl_ASP.NET.Models
         public int Id { get; set; }
         [StringLength(maximumLength: 50, ErrorMessage = "This field cannot be longer than {1} characters")]
         public string Name { get; set; }
+        [Display(Name = "Operation Type")]
         public OperationType OperationTypeId { get; set; }
         public int UserId { get; set; }
     }
