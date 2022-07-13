eb0d2f38ad8bde2b1735f4f4617b7f9997af634c
luisJavierLuna <javierlunaluis@gmail.com>
2022-07-13 14:20:08 -0500

    Added Update and Edit function for the categories, P1: added controller

--- ExpenseControl_ASP.NET/Controllers/CategoriesController.cs
+++ ExpenseControl_ASP.NET/Controllers/CategoriesController.cs
@@ -44,5 +44,40 @@ namespace ExpenseControl_ASP.NET.Controllers
             await categoriesRepository.Create(category);
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var userId = usersService.GetUserId();
+            var category = await categoriesRepository.GetById(id, userId);
+
+            if (category is null)
+            {
+                return RedirectToAction("ElementNotFound", "Home");
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Category categoryToEdit)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(categoryToEdit);
+            }
+
+            var userId = usersService.GetUserId();
+            var category = await categoriesRepository.GetById(categoryToEdit.Id, userId);
+
+            if (category is null)
+            {
+                return RedirectToAction("ElementNotFound", "Home");
+            }
+
+            categoryToEdit.UserId= userId;
+            await categoriesRepository.Update(categoryToEdit);
+            return RedirectToAction("Index");
+        }
+
     }
 }
