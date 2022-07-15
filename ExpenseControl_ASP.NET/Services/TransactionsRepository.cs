f601ff59667641d40eac3a9d12b0681dd49f128c
luisJavierLuna <javierlunaluis@gmail.com>
2022-07-15 17:44:51 -0500

    Added Create Transaction function: added repository method with store procedure

---
+++ ExpenseControl_ASP.NET/Services/TransactionsRepository.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using ExpenseControl_ASP.NET.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ExpenseControl_ASP.NET.Services
+{
+    public interface ITransactionsRepository
+    {
+
+    }
+
+    public class TransactionsRepository: ITransactionsRepository
+    {
+        private readonly string connectionString;
+
+        public TransactionsRepository(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(Transaction transaction)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(
+                "Transactions_Insert",
+                new
+                {
+                    transaction.UserId,
+                    transaction.TransactionDate,
+                    transaction.Amount,
+                    transaction.CategoryId,
+                    transaction.AccountId,
+                    transaction.Note
+                },
+                commandType: System.Data.CommandType.StoredProcedure);
+
+            transaction.Id = id;
+        }
+
+    }
+}
