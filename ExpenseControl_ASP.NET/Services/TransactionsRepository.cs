249ed13c9bbee3fd6da04c5b8967421f862265fe
luisJavierLuna <javierlunaluis@gmail.com>
2022-07-22 15:02:00 -0500

    Added Update Transaction function, P1. Repository only

--- ExpenseControl_ASP.NET/Services/TransactionsRepository.cs
+++ ExpenseControl_ASP.NET/Services/TransactionsRepository.cs
@@ -7,6 +7,7 @@ namespace ExpenseControl_ASP.NET.Services
     public interface ITransactionsRepository
     {
         Task Create(Transaction transaction);
+        Task Update(Transaction transaction, decimal previousAmount, int previousAccount);
     }
 
     public class TransactionsRepository: ITransactionsRepository
@@ -37,5 +38,26 @@ namespace ExpenseControl_ASP.NET.Services
             transaction.Id = id;
         }
 
+        public async Task Update(
+            Transaction transaction,
+            decimal previousAmount,
+            int previousAccount)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(
+                "Transactions_Update",
+                new
+                {
+                    transaction.Id,
+                    transaction.TransactionDate,
+                    transaction.Amount,
+                    transaction.CategoryId,
+                    transaction.AccountId,
+                    transaction.Note,
+                    previousAmount,
+                    previousAccount
+                },
+                commandType: System.Data.CommandType.StoredProcedure);
+        }
     }
 }
