c978fb4cce49071ee841122dd2adc6015f4bd56b
luisJavierLuna <javierlunaluis@gmail.com>
2022-07-24 13:39:24 -0500

    Isolated Partial View Code from the view Create Transaction in order to use it in several views

---
+++ ExpenseControl_ASP.NET/wwwroot/js/TransactionsForm.js
@@ -0,0 +1,20 @@
+﻿function initializeTransactionForm(urlGetCategories) {
+    $("#OperationTypeId").change(async function () {
+        const selectedValue = $(this).val();
+
+        const response = await fetch(urlGetCategories, {
+            method: 'POST',
+            body: selectedValue,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const json = await response.json();
+        const options =
+            json.map(category => `<option value=${category.value}>${category.text}</option>`);
+        $("#CategoryId").html(options);
+
+    })
+}
+
