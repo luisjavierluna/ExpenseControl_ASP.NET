3bdf5a349a6cd2b442ae54e803e834434cfa74c8
luisJavierLuna <javierlunaluis@gmail.com>
2022-07-26 11:13:26 -0500

    Group Transactions by Account feature, P1: Detailed Report Model

---
+++ ExpenseControl_ASP.NET/Models/DetailedTransactionsReport.cs
@@ -0,0 +1,25 @@
+﻿namespace ExpenseControl_ASP.NET.Models
+{
+    public class DetailedTransactionsReport
+    {
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public IEnumerable<TransactionsByDate> GroupedTransactions { get; set; }
+        public decimal DepositsBalance => GroupedTransactions.Sum(x => x.DepositsBalance);
+        public decimal WithdrawalsBalance => GroupedTransactions.Sum(x => x.WithdrawalsBalance);
+        public decimal Total => DepositsBalance - WithdrawalsBalance;
+
+        public class TransactionsByDate
+        {
+            public DateTime TransactionDate { get; set; }
+            public IEnumerable<Transaction> Transactions { get; set; }
+            public decimal DepositsBalance =>
+                Transactions.Where(x => x.OperationTypeId == OperationType.Income)
+                .Sum(x => x.Amount);
+            public decimal WithdrawalsBalance =>
+                Transactions.Where(x => x.OperationTypeId == OperationType.Expense)
+                .Sum(x => x.Amount);
+        }
+
+    }
+}
