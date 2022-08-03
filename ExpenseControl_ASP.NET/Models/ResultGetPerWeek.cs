6a98bfa730da4597f5e77139ed17577939ff89ef
luisjavierluna <javierlunaluis@gmail.com>
2022-08-03 13:17:27 -0500

    Added some UI improvements in Transactions Index View and added model for weekly report

---
+++ ExpenseControl_ASP.NET/Models/ResultGetPerWeek.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseControl_ASP.NET.Models
+{
+    public class ResultGetPerWeek
+    {
+        public int Week { get; set; }
+        public decimal Amount { get; set; }
+        public OperationType OperationTypeId { get; set; }
+
+    }
+}
