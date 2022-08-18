af7092e59c82c25c205c51ce34d8d13c15db559b
luisjavierluna <javierlunaluis@gmail.com>
2022-08-18 10:01:52 -0500

    Added controller for sign up feature

---
+++ ExpenseControl_ASP.NET/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using ExpenseControl_ASP.NET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseControl_ASP.NET.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("Index", "Transactions");
+        }
+    }
+
+}
+}
