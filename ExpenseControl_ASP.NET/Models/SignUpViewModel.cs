2789158b1b43f6e0d782cc60e6320d8a92c1ee49
luisjavierluna <javierlunaluis@gmail.com>
2022-08-18 09:56:55 -0500

    Added model for sign up feature

---
+++ ExpenseControl_ASP.NET/Models/SignUpViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseControl_ASP.NET.Models
+{
+    public class SignUpViewModel
+    {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "This field must be a valid Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "The field {0} is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
