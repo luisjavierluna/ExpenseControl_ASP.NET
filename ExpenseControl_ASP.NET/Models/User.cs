66788615b8684d4684045da30342b2d1224d48ed
luisjavierluna <javierlunaluis@gmail.com>
2022-08-22 09:58:30 -0500

    Added user model for the Users System

---
+++ ExpenseControl_ASP.NET/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseControl_ASP.NET.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string EmailNormalizado { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
