3c329000541f889b6ff12e06996a598cfca12926
luisjavierluna <javierlunaluis@gmail.com>
2022-08-31 11:10:09 -0500

    Added function to count all the categories records in DB. Model

---
+++ ExpenseControl_ASP.NET/Models/PaginationAnswer.cs
@@ -0,0 +1,17 @@
+﻿namespace ExpenseControl_ASP.NET.Models
+{
+    public class PaginationAnswer
+    {
+        public int Page { get; set; } = 1;
+        public int RecordsPerPage { get; set; } = 10;
+        public int TotalRecordsQuantity { get; set; }
+        public int TotalPageQuantity => (int)Math.Ceiling((double)TotalRecordsQuantity / RecordsPerPage);
+        public string BaseURL { get; set; }
+
+    }
+    public class PaginationAnswer<T> : PaginationAnswer
+    {
+        public IEnumerable<T> Elements { get; set; }
+    }
+
+}
